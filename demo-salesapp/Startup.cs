774434e2726c524ef1821690ccf2dd9a25bdb43a
Sami Lamti <sami.lamti@if.se>
2015-12-03 14:27:40 +0100

    Initial commit

---
+++ demo-salesapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(demo_salesapp.Startup))]
+namespace demo_salesapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
